bd2617599cc3d2e7354c38d4269ada98a7855a96
sajjad <assassinsajjad0@gmail.com>
2022-02-26 21:12:56 +0330

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Instagram
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textSearch_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0)
+                textSearch.Visibility = Visibility.Collapsed;
+            else
+                textSearch.Visibility = Visibility.Visible;
+        }
+
+    }
+}
---
+++ UserControls/Post.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Instagram.UserControls
+{
+    public partial class Post : UserControl
+    {
+        public Post()
+        {
+            InitializeComponent();
+        }
+
+
+        public ImageSource PostSource
+        {
+            get { return (ImageSource)GetValue(PostSourceProperty); }
+            set { SetValue(PostSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PostSourceProperty = DependencyProperty.Register("PostSource", typeof(ImageSource), typeof(Post));
+
+
+        public ImageSource ProfileSource
+        {
+            get { return (ImageSource)GetValue(ProfileSourceProperty); }
+            set { SetValue(ProfileSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileSourceProperty = DependencyProperty.Register("ProfileSource", typeof(ImageSource), typeof(Post));
+
+
+        public string ProfileName
+        {
+            get { return (string)GetValue(ProfileNameProperty); }
+            set { SetValue(ProfileNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileNameProperty = DependencyProperty.Register("ProfileName", typeof(string), typeof(Post));
+
+
+        public string Likes
+        {
+            get { return (string)GetValue(LikesProperty); }
+            set { SetValue(LikesProperty, value); }
+        }
+
+        public static readonly DependencyProperty LikesProperty = DependencyProperty.Register("Likes", typeof(string), typeof(Post));
+
+
+        public string Comments
+        {
+            get { return (string)GetValue(CommentsProperty); }
+            set { SetValue(CommentsProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommentsProperty = DependencyProperty.Register("Comments", typeof(string), typeof(Post));
+
+    }
+}
